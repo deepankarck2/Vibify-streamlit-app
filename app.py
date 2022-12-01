4240424208cd90475ab769667c278cf0599b04de
Deepankar <dip2leo@gmail.com>
2022-12-01 17:48:49 -0500

    nav

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+from streamlit_option_menu import option_menu 
 
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+	
+selected2
 txt = st.text_area('Please input test:', placeholder="Placeholder", height=140)
 
 if(st.button('Submit')):
